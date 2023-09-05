599b06076ef17ed718c02d88996f3f6d077b5f16
kima-s <kimasyun@gmail.com>
2023-09-05 18:13:56 +0900

    first commit

---
+++ src/test/java/com/example/NotificationManagementAPI/NotificationManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NotificationManagementAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
