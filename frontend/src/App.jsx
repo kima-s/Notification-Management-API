e8bcb7a4c47ce64a152cf8862157fed9f7a5e608
kima-s <kimasyun@gmail.com>
2023-09-13 22:38:16 +0900

    フロントエンドの登録処理を実装

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Router } from './router/Router';
+
+function App() {
+  return (
+    <ChakraProvider>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ChakraProvider>
+  );
+}
+
+export default App;
